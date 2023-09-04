d159589bbe36536eed8a85cfbfd1d2cb361453be
SirJShostakiv <sshv2013@gmail.com>
2023-09-04 18:08:49 +0500

    created POJO class and bean Cat, then configurated and tested successfully

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWorldBean1.getMessage());
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldBean2.getMessage());
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getName());
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getName());
+        System.out.println(helloWorldBean1 == helloWorldBean2);
+        System.out.println(catBean1 == catBean2);
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,23 @@
+public class Cat {
+    private String name;
+    private String sex;
+
+    public Cat() {
+    }
+    public Cat(String name, String sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
